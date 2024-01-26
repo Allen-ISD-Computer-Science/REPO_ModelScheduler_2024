21f64f938de0763844a477274711eb6516a1343d
Tinpot <111015952+darrnguyen@users.noreply.github.com>
2024-01-26 00:28:31 +0000

    Fixed problem where part of the UI would go off screen

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -7,7 +7,7 @@ const Homepage = () => {
     <>
       {/* Contains link to: FAQ, Classes, Scheduler(?), Feedback */}
       <HomepageLayout>
-      <div style={{backgroundColor:"#1F1F1F"}} className="w-52 h-screen mr-auto">
+      <div style={{backgroundColor:"#1F1F1F"}} className="w-52 h-full mr-auto">
         <div className="flex justify-center ">
             <Image
               src={AllenLogo}
@@ -21,13 +21,13 @@ const Homepage = () => {
           Model Scheduler
         </div>
 
-        <div className="mx-auto w-10/12 relative top-2/3 text-3xl font-mono text-center rounded-lg tracking-tighter space-y-10">
-          <div className="bg-neutral-700 rounded-lg">Feedback</div>
-          <div style={{aspectRatio: "1/1", height: 75, width: 75}} className="mx-auto bg-neutral-700 rounded-full py-5">?</div>
+        <div className="flex justify-center mx-auto w-10/12 text-3xl font-mono text-center rounded-lg tracking-tighter">
+          <div className="bg-neutral-700 rounded-lg absolute bottom-40">Feedback</div>
+          <div style={{aspectRatio: "1/1", height: 75, width: 75}} className="mx-auto bg-neutral-700 absolute bottom-10 rounded-full py-5">?</div>
         </div>
       </div>
 
-      <div className="w-screen h-screen font-mono">
+      <div className="w-full h-full font-mono">
         <div className="text-center text-5xl p-8">Stuff goes here</div>
         <div className="mx-auto text-center bg-neutral-900 w-1/2 h-5/6 rounded-lg shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_50px_#08f] pt-10">
           <div className="mx-auto text-center bg-neutral-700 w-5/6 h-1/6 rounded-lg">
