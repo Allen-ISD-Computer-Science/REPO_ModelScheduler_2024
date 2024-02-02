e11c2d3e84c3e8440b4624823c778bbed8c26ed9
Tinpot <111015952+darrnguyen@users.noreply.github.com>
2024-02-02 16:55:13 +0000

    redesigned home page, made buttons work #20

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -2,57 +2,68 @@ import { HomepageLayout } from "@/components/Layout";
 import { Image } from "@nextui-org/image";
 import AllenLogo from "../components/Images/AllenISDLogo.png";
 
-const Homepage = () => {
+import { Button } from "@nextui-org/button";
+import { useNavigate } from "react-router-dom";
+
+export default function Homepage() {
+  const navigate = useNavigate()
+
+  const NavigateToPage = (Location) => {
+    const page = Location
+    navigate("/" + page.toString())
+  }
+
   return (
     <>
       {/* Contains link to: FAQ, Classes, Scheduler(?), Feedback */}
       <HomepageLayout>
-      {/* <div style={{backgroundColor:"#1F1F1F"}} className="w-52 h-full mr-auto">
-        <div className="flex justify-center ">
-            <Image
-              src={AllenLogo}
-              alt="AllenISD Logo"
-              className="mt-5 mx-auto my-auto"
-              style={{blockSize: 120}}
-            />
-        </div>
 
-        <div className="mx-auto w-10/12 mt-10 text-3xl font-mono text-center rounded-lg tracking-tighter bg-neutral-700">
-          AHS Model Scheduler
-        </div>
+      <div className="w-full h-full absolute bottom-0 flex place-items-center flex-col space-y-10 pt-40 font-sans font-thin tracking-wide">
+        <Image
+          src={AllenLogo}
+          alt="AllenISD Logo"
+          className="my-auto mx-auto"
+          style={{blockSize: 300}}
+        />
 
-        <div className="flex justify-center mx-auto w-10/12 text-3xl font-mono text-center rounded-lg tracking-tighter">
-          <div className="bg-neutral-700 rounded-lg absolute bottom-40">Feedback</div>
-          <div style={{aspectRatio: "1/1", height: 75, width: 75}} className="mx-auto bg-neutral-700 absolute bottom-10 rounded-full py-5">?</div>
+        <div className="flex place-items-center justify-center mx-auto w-9/12 text-center rounded-lg tracking-wide">
+          <Button className="bg-neutral-700 p-8 text-3xl font-thin font-sans" onPress={() => NavigateToPage("scheduler")}>
+            Schedule
+          </Button>
         </div>
-      </div>
-
-      <div className="w-full h-full font-mono">
-        <div className="text-center text-5xl p-8">Welcome</div>
-        <div className="mx-auto text-center bg-neutral-900 w-1/2 h-5/6 rounded-lg shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_50px_#08f] pt-10">
+        
+        <div className="mx-auto text-center bg-neutral-900 w-1/2 h-3/6 rounded-lg shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_50px_#08f] pt-10">
           <div className="mx-auto text-center bg-neutral-700 w-5/6 h-1/6 rounded-lg">
-            <div className="pt-2 text-3xl">Stuff goes here</div>
+          <div className="pt-2 text-3xl">Stuff goes here</div>
           </div>
         </div>
-      </div> */}
 
-      <div style={{backgroundColor:"#1F1F1F"}} className="w-full h-40 flex justify-center mb-auto font-sans font-thin">
-      <div className="flex place-items-center justify-center mx-auto w-10/12 text-3xl text-center rounded-lg tracking-wide space-x-40">
-          <div className="bg-neutral-700 rounded-lg p-4">Placeholder</div>
-          <div className="bg-neutral-700 rounded-lg p-4">Placeholder</div>
+      </div>
+
+      <div style={{backgroundColor:"#1F1F1F"}} className="w-full h-32 flex justify-center mb-auto font-sans font-thin">
+      <div className="flex place-items-center justify-center mx-auto w-10/12 text-3xl text-center rounded-lg tracking-wide space-x-36">
+          {/*<div className="bg-neutral-700 rounded-lg p-4 w-1/4">Classes</div>*/}
+          <Button className="bg-neutral-700 p-8 w-1/4 font-sans font-thin tracking-wide text-3xl" onPress={() => NavigateToPage("classes")}>
+            Classes
+          </Button>
+          <Button className="bg-neutral-700 p-8 w-1/4 font-sans font-thin tracking-wide text-3xl" onPress={() => NavigateToPage("guide")}>
+            Guide
+          </Button>
         </div>
 
         <div className="flex place-items-center tracking-wide text-8xl font-sans font-thin">Welcome</div>
 
         <div className="flex place-items-center justify-center mx-auto w-10/12 text-3xl text-center rounded-lg tracking-wide space-x-40">
-          <div className="bg-neutral-700 rounded-lg p-4">Placeholder</div>
-          <div className="bg-neutral-700 rounded-lg p-4">Placeholder</div>
+        <Button className="bg-neutral-700 p-8 w-1/4 font-sans font-thin tracking-wide text-3xl" onPress={() => NavigateToPage("faq")}>
+            FAQ
+          </Button>
+          <Button className="bg-neutral-700 p-8 w-1/4 font-sans font-thin tracking-wide text-3xl" onPress={() => NavigateToPage("feedback")}>
+            Feedback
+          </Button>
         </div>
       </div>
 
       </HomepageLayout>
     </>
   );
-};
-
-export default Homepage;
+}
