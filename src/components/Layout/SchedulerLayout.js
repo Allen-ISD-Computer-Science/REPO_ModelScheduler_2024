4611edd73962fd58ac4aa50a712feeb4b62269b3
Reynard G <40525094+Reynard-G@users.noreply.github.com>
2024-02-11 05:18:01 +0000

    Update background color in ClassesLayout and SchedulerLayout

--- src/components/Layout/SchedulerLayout.js
+++ src/components/Layout/SchedulerLayout.js
@@ -1,7 +1,7 @@
 export default function SchedulerLayout({ children }) {
   return (
     <>
-      <div className="flex flex-col md:flex-row overflow-y-auto items-center justify-evenly h-screen w-screen px-8 md:px-12">
+      <div className="flex flex-col md:flex-row overflow-y-auto items-center justify-evenly h-screen w-screen bg-default-200/20 px-8 md:px-12">
         {children}
       </div>
     </>
