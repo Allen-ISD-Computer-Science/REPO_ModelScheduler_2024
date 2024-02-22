1a6f5659f5be0b1d7490c69314bb2e2db2bab28f
Reynard G <40525094+Reynard-G@users.noreply.github.com>
2024-02-22 15:02:05 +0000

    Edited min-height for all pages

--- src/components/Layout/ReviewLayout.js
+++ src/components/Layout/ReviewLayout.js
@@ -1,7 +1,7 @@
 export default function ReviewLayout({ children }) {
   return (
     <>
-      <div className="flex flex-col overflow-y-auto md:flex-row items-center justify-evenly h-screen w-screen bg-default-200/20 px-8 md:px-12 gap-8">
+      <div className="flex flex-col overflow-y-auto md:flex-row items-center justify-evenly bg-default-200/20 px-8 md:px-12 gap-8 h-[calc(100vh-4rem)]">
         {children}
       </div>
     </>
