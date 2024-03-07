ed4c1f7b64ba7f81119c3090cae89dbd9eabaf1f
Willy <swiftcslol@gmail.com>
2024-03-07 10:36:40 -0600

    Added countdown timer to test.js page

--- frontend/src/pages/test.js
+++ frontend/src/pages/test.js
@@ -1,16 +1,37 @@
 import { DefaultNavbar } from "@/components/Navbars";
 import TestLayout from "@/components/Layout/TestLayout";
+import { useEffect, useState } from "react";
 
-export default function FAQ() {
+export default function Test() {
+    const [countdown, setCountdown] = useState("");
+    useEffect(() => {
+        var targetDate = new Date("March 25, 2024 07:30:00");
+        function updateCountdown() {
+            var currentDate = new Date();
+            var timeDifference = targetDate.getTime() - currentDate.getTime();
+            var days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+            setCountdown(days + "d " + hours + "h " + minutes + "m " + seconds + "s");
+	}
+
+	updateCountdown();
+        const countdownInterval = setInterval(updateCountdown, 1000);
+        return () => clearInterval(countdownInterval);
+    }, []);
+
     return (
-      <>
-        <DefaultNavbar />
+	    <>
+	       <DefaultNavbar />
 
-        <TestLayout>
-            <div>
-                <h2>Test</h2>
-            </div>
-        </TestLayout>
-      </>
+	       <TestLayout>
+                 <div>
+                   <h2>Test</h2>
+                   <h2>Junior Priority 1 Countdown</h2>
+                   <p>{countdown}</p>
+                   </div>
+               </TestLayout>
+	    </>
     );
 }
