74674719e90b630f47818ed8b9ec7e0a2f0a7a9d
Tinpot <111015952+darrnguyen@users.noreply.github.com>
2024-03-08 04:19:21 +0000

    Added priority timers for all groups in the testing page

--- frontend/src/pages/test.js
+++ frontend/src/pages/test.js
@@ -1,11 +1,14 @@
-import DefaultNavbar from "@/components/Navbars";
+import DefaultNavbar from "@/components/Navbars/DefaultNavbar";
 import TestLayout from "@/components/Layout/TestLayout";
 import { useEffect, useState } from "react";
 
 export default function Test() {
+  
+  function SetDate(selectedDate) {
     const [countdown, setCountdown] = useState("");
     useEffect(() => {
-        var targetDate = new Date("March 25, 2024 07:30:00");
+        //var targetDate = new Date("March 25, 2024 07:30:00");
+        var targetDate = selectedDate
         function updateCountdown() {
             var currentDate = new Date();
             var timeDifference = targetDate.getTime() - currentDate.getTime();
@@ -20,6 +23,10 @@ export default function Test() {
         const countdownInterval = setInterval(updateCountdown, 1000);
         return () => clearInterval(countdownInterval);
     }, []);
+    return (
+      <p>{countdown}</p>
+    );
+  }
 
   return (
     <>
@@ -28,9 +35,31 @@ export default function Test() {
 
     <TestLayout>
         <div>
-            <h2>Test</h2>
+            <h1 className="flex justify-center text-3xl">THIS IS A TEST PAGE FOR POTENTIALLY NEW FEATURES</h1>
+            <br></br>
             <h2>Junior Priority 1 Countdown</h2>
-            <p>{countdown}</p>
+            <p>{SetDate(new Date("March 25, 2024 07:30:00"))}</p>
+            <br></br>
+            <h2>Junior Priority 2 Countdown</h2>
+            <p>{SetDate(new Date("March 27, 2024 07:30:00"))}</p>
+            <br></br>
+            <h2>Junior Group 3 Countdown</h2>
+            <p>{SetDate(new Date("April 1, 2024 07:30:00"))}</p>
+            <br></br>
+            <h2>Sophomore Priority 1 Countdown</h2>
+            <p>{SetDate(new Date("April 3, 2024 07:30:00"))}</p>
+            <br></br>
+            <h2>Sophomore Priority 2 Countdown</h2>
+            <p>{SetDate(new Date("April 5, 2024 07:30:00"))}</p>
+            <br></br>
+            <h2>Sophomore Group 3 Countdown</h2>
+            <p>{SetDate(new Date("April 9, 2024 07:30:00"))}</p>
+            <br></br>
+            <h2>Freshmen Group 1 Countdown</h2>
+            <p>{SetDate(new Date("April 11, 2024 07:30:00"))}</p>
+            <br></br>
+            <h2>Freshmen Group 2 Countdown</h2>
+            <p>{SetDate(new Date("April 15, 2024 07:30:00"))}</p>
         </div>
     </TestLayout>
 
